7d658379f04bd660c81e64f7675ddf16f319cae3
Ricardo Espinosa <rfespinosa7@utpl.edu.ec>
2024-05-21 14:58:39 -0500

    Proyecto vacio

---
+++ Solucion_Codigo/Taller5_6_Grupal/src/ejercicio1.java
@@ -0,0 +1,4 @@
+//Autores: Juan Diego Guerrero y Ricardo Espinosa
+public class ejercicio1 {
+    //Clase de prueba
+}
---
+++ Solucion_Codigo/Taller5_6_Grupal/src/ejercicio2.java
@@ -0,0 +1,4 @@
+//Autores: Juan Diego Guerrero y Ricardo Espinosa
+public class ejercicio2 {
+    //Clase de pueba
+}
